1bc7cd8eee5c8e7f632f96ed8fe01cc969d50954
arostron <alex.rostron24@gmail.com>
2020-04-30 16:55:16 -0600

    Refactor for better code quality

--- stats-gen/genStats.py
+++ stats-gen/genStats.py
@@ -1,26 +1,23 @@
-from random import randint as r
+from random import randint
+import argparse
 
-def rollDice(d=6):
-    return r(1,d)
+roll_d = lambda x: randint(1,x)
 
-def genStat(n=4, d=6):
-    # roll and sum n dice then subtract minimum
-    total = 0
-    rolls = []
-    for i in range(n):
-        r = rollDice(d);
-        total += r
-        rolls.append(r)
-    #print("Rolled: ", rolls)
-    total-= min(rolls)
-    return total
+def top_3_of_4_d6():
+	rolls = [roll_d(6) for i in range(4)]
+	return sum(rolls) - min(rolls)
 
-def genStats(n=6):
-    stats = []
-    for i in range(n):
-        stats.append(genStat())
-    return sorted(stats,reverse=True)
+def player_stats():
+	return sorted([ top_3_of_4_d6() for i in range(6)], reverse=True)
 
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-f', help='Output filename', type=str, default="stats.txt")
+	parser.add_argument('-n', type=int, default=1, help='How many player stats to generate')
+	args = parser.parse_args()
 
-print("Here are your standard DnD stats")
-print(genStats())
+	with open(args.f, 'a') as f:
+		for i in range(args.n):
+			stats = player_stats()
+			f.write("{}\n".format(stats))
+			print("Stats: ", stats)
